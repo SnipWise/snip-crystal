f4901cd2d65895589091cbea115f7f3bbeea40e3
k33g <ph.charriere@gmail.com>
2026-01-03 17:41:21 +0100

    🛟 Updated.

--- workspace/hello.cr
+++ workspace/hello.cr
@@ -1,13 +1,7 @@
-require "io
+puts "Hello, Crystal!"
 
-begin
-  puts "Please enter your name:"
-  input = gets.chomp
-  if input
-    puts "Hello, #{input}!"
-  else
-    puts "No input provided."
-  end
-rescue StandardError
-  puts "An error occurred while reading input."
-end
+# Create a hash
+person = {name: "Alice", age: 30}
+
+# Print the hash
+puts person
